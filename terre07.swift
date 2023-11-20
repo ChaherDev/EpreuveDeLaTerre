1e61e83943512c42913e0a968aa6857c9ef4327c
Chaher <chaher.croissance.public@gmail.com>
2023-11-20 10:03:01 +0100

    terre07.swift Completed

---
+++ terre07.swift
@@ -0,0 +1,16 @@
+// Taille du'une chaine
+
+// il faut que les nombres renvoient une erreur
+
+func size() {
+    let array = Array(CommandLine.arguments[1])
+    print(array.count)
+}
+
+if CommandLine.arguments.count != 2 {
+    print("Vous devez fournir un argument")
+} else if Int(CommandLine.arguments[1]) != nil {
+    print("Vous ne devez pas fournir de nombre")
+} else {
+    size()
+}

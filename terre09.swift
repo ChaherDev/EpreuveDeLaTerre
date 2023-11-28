7e1df22b7e8ca4769329ecb89d555ef298159d6c
Chaher <chaher.croissance.public@gmail.com>
2023-11-28 08:46:39 +0100

    Update

--- terre09.swift
+++ terre09.swift
@@ -1,13 +1,30 @@
 // Racine carré d'un nombre
 
-func sqrt() {
-
+func squarreRoot() {
+    let number = Int(CommandLine.arguments[1])!
+    if number == 0 {
+        print(0)
+    } else if number == 1 {
+        print(1)
+    } else {
+        for i in 1...number {
+            if i * i >= number {
+                if i * i == number {
+                    print(i)
+                    break
+                } else {
+                    print(i - 1)
+                    break
+                }
+            }
+        }
+    }
 }
 
 if CommandLine.arguments.count != 2 {
-    print("Vous devez fournir un seul argument")
+    print("Vous devez fournir un argument")
 } else if Int(CommandLine.arguments[1]) == nil {
     print("Vous devez fournir un nombre entier")
 } else {
-    sqrt()
+    squarreRoot()
 }

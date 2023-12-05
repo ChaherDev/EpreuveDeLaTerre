d257aeb2ccb299d2d8281151ca71394d035cf032
Chaher <chaher.croissance.public@gmail.com>
2023-12-05 16:30:24 +0100

    Update

--- terre10.swift
+++ terre10.swift
@@ -1,1 +1,15 @@
 // Nombre premier
+
+func primeNumber() {
+    if let argument = Int(CommandLine.argument[1]) {
+        if argument % 
+    }
+}
+
+if CommandLine.arguments.count != 2 {
+    print("Vous devez fournir un seul argument")
+} else if let argument = Int(CommandLine.arguments[1]) {
+    primeNumber()
+} else {
+    print("Vous devez fournir un nombre entier")
+}

c80a745bd92bfdfe1c8b425586edebfe4278280b
Chaher <chaher.croissance.public@gmail.com>
2023-12-12 14:57:03 +0100

    terre10.swift Completed

--- terre10.swift
+++ terre10.swift
@@ -1,15 +1,31 @@
 // Nombre premier
 
-func primeNumber() {
-    if let argument = Int(CommandLine.argument[1]) {
-        if argument % 
+// Un nombre premier est un nombre entier positif
+// Donc on peut éliminer des cas
+
+func primeNumber() -> Bool {
+    if let argument = Int(CommandLine.arguments[1]) {
+        for i in 2..<argument {
+            if argument % i == 0 {
+                return false
+            }
+        }
     }
+    return true
 }
 
 if CommandLine.arguments.count != 2 {
     print("Vous devez fournir un seul argument")
 } else if let argument = Int(CommandLine.arguments[1]) {
-    primeNumber()
+    if argument < 0 {
+        print("Vous devez fournir un entier positif")
+    } else if argument == 0 || argument == 1 {
+        print("Non, \(argument) n'est pas un nombre premier")
+    } else if argument == 2 || primeNumber() {
+        print("Oui, \(argument) est un nombre premier")
+    } else {
+        print("Non, \(argument) n'est pas un nombre premier")
+    }
 } else {
     print("Vous devez fournir un nombre entier")
 }

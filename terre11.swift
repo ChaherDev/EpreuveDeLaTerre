aa4f9d45403bc672894d6d880bb1c9fd2376cad3
Chaher <chaher.croissance.public@gmail.com>
2023-12-28 09:33:55 +0100

    Update

---
+++ terre11.swift
@@ -0,0 +1,16 @@
+// 24 to 12
+
+// il n y a pas l'air d'avoir d'espaces donc c'est un seul argument
+
+// Donc il faut que je transforme mon argument en un tableau de 5 éléments
+// Attention à midi et minuit
+
+func 
+
+if CommandLine.arguments != 2 {
+    print("Vous devez fournir un seul argument")
+} else if {
+    print("Votre argument doit être de la forme : 13:45")
+} else {
+    
+}

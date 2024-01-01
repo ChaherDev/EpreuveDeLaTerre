befd0198455cbd3f544c06fca68b8833c6234ceb
Chaher <chaher.croissance.public@gmail.com>
2024-01-01 18:34:27 +0100

    Add .gitignore

--- terre11.swift
+++ terre11.swift
@@ -1,11 +1,8 @@
 // 24 to 12
 
-// il n y a pas l'air d'avoir d'espaces donc c'est un seul argument
+func hoursConvertor() {
 
-// Donc il faut que je transforme mon argument en un tableau de 5 éléments
-// Attention à midi et minuit
-
-func 
+}
 
 if CommandLine.arguments != 2 {
     print("Vous devez fournir un seul argument")

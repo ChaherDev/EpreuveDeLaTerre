92a5f72d310b360db682f63692cc15947bfa5a06
Chaher <chaher.croissance.public@gmail.com>
2024-01-02 12:19:30 +0100

    Add files

--- terre11.swift
+++ terre11.swift
@@ -1,7 +1,15 @@
 // 24 to 12
 
 func formatChecker() -> Bool {
-    
+    let userInput = Array(CommandLine.arguments[1])
+    if userInput.count != 5 {
+        return false
+    } else if userInput[2] != ":" {
+        return false
+    } else if userInput[0] != "0" && userInput[0] != "1" && userInput[0] != "2" {
+        return false
+    } else if userInput[1] !=
+
 }
 
 func hoursConvertor() {

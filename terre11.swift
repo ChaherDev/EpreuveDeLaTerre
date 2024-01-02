a7172e7a9d3ecc628853e24b920fe92f4afece3b
Chaher <chaher.croissance.public@gmail.com>
2024-01-02 12:19:04 +0100

    Add files

--- terre11.swift
+++ terre11.swift
@@ -1,13 +1,17 @@
 // 24 to 12
 
+func formatChecker() -> Bool {
+    
+}
+
 func hoursConvertor() {
 
 }
 
 if CommandLine.arguments != 2 {
     print("Vous devez fournir un seul argument")
-} else if {
+} else if !formatChecker {
     print("Votre argument doit être de la forme : 13:45")
 } else {
-    
+    hoursConvertor()
 }
